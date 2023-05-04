2cbb45940dd4cd2d4cb5a1460a6af408ce6fc029
Ye <yebh3@mail2.sysu.edu.cn>
2023-05-04 21:15:24 +0800

    test

--- 1_train.sh
+++ 1_train.sh
@@ -1,31 +1,31 @@
 gpu=1
 
-configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T03-a1-cfa11.py
-work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T03-a1-cfav11-ohem
-work_dir=${work_dir}-gpu${gpu}-v1
-python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
-python read_json_and_save_top50.py --path=${work_dir}
+# configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T03-a1-cfa11.py
+# work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T03-a1-cfav11-ohem
+# work_dir=${work_dir}-gpu${gpu}-v1
+# python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
+# python read_json_and_save_top50.py --path=${work_dir}
 
-configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T03-a1-cfa11.py
-work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T03-a1-cfav11-ohem
-work_dir=${work_dir}-gpu${gpu}-v2
-python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
-python read_json_and_save_top50.py --path=${work_dir}
-# t05
-configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfa11.py
-work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfav11-ohem
-work_dir=${work_dir}-gpu${gpu}-v0
-python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
-python read_json_and_save_top50.py --path=${work_dir}
+# configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T03-a1-cfa11.py
+# work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T03-a1-cfav11-ohem
+# work_dir=${work_dir}-gpu${gpu}-v2
+# python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
+# python read_json_and_save_top50.py --path=${work_dir}
+# # t05
+# configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfa11.py
+# work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfav11-ohem
+# work_dir=${work_dir}-gpu${gpu}-v0
+# python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
+# python read_json_and_save_top50.py --path=${work_dir}
 
-configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfa11.py
-work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfav11-ohem
-work_dir=${work_dir}-gpu${gpu}-v1
-python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
-python read_json_and_save_top50.py --path=${work_dir}
+# configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfa11.py
+# work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfav11-ohem
+# work_dir=${work_dir}-gpu${gpu}-v1
+# python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
+# python read_json_and_save_top50.py --path=${work_dir}
 
-configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfa11.py
-work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfav11-ohem
-work_dir=${work_dir}-gpu${gpu}-v2
-python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
-python read_json_and_save_top50.py --path=${work_dir}
+# configs=/home/yebh/mmdetection/configs/uda_608/100e/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfa11.py
+# work_dir=work_dirs/test_dataset/L2T_SCL/CFA_608/uda_yolov3_L2T_SCL-100e-110-0001-T05-a1-cfav11-ohem
+# work_dir=${work_dir}-gpu${gpu}-v2
+# python tools/train.py $configs --gpu-id=${gpu} --work-dir=${work_dir} --auto-scale-lr --seed=1079546523 
+# python read_json_and_save_top50.py --path=${work_dir}
