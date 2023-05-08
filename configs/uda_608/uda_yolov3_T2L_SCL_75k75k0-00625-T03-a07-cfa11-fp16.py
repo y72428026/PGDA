b9e0baaa544802ca5f452b96dfa07812ba1c447e
Ye <yebh3@mail2.sysu.edu.cn>
2023-05-08 16:33:38 +0800

    upload for other servers

---
+++ configs/uda_608/uda_yolov3_T2L_SCL_75k75k0-00625-T03-a07-cfa11-fp16.py
@@ -0,0 +1,19 @@
+_base_ = [  '../yolo/uda_yolov3_608_T2L_SCL-000-0-fp16.py', ]
+
+
+uda=dict(
+    da_head=dict(
+        img_weight=7500  
+    ),
+    da_ano_head=dict(
+        img_weight=7500
+    ),
+    da_pred_head=dict(
+        img_weight=0
+    ),
+    cfa_thres=0.3,
+    enable_category_loss=True,
+    category_weight=0.0625,
+    alpha_mb=0.7,
+    cfa_v=11
+    )
